4fa667cb05d9a915a0f19eb5bebaaf58ee61f06c
saharrrrr <skmao@gmail.com>
2023-09-08 17:42:33 -0700

    c commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: print a-z in reverse
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+
+	return (0);
+}
