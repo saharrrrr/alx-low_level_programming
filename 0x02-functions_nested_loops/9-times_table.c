a6ec0b8b96d4f5ca06fb16fd0a11c578ca366cc8
saharrrrr <skmao@gmail.com>
2023-09-12 14:43:47 -0700

    c function commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,8 +21,8 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + 48);
-				_putchar((prod % 10) + 48);
+				_putchar((prod / 10) + 48); /*get the first digit*/
+			_putchar((prod % 10) + 48); /*fet the second digit*/
 		}
 		_putchar('\n');
 	}
