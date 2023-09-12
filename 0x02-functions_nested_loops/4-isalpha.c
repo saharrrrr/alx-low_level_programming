b6a81d5ae9f8d91c91633425f84682e1adecd8d9
saharrrrr <skmao@gmail.com>
2023-09-12 11:54:22 -0700

    c function commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * _isalpha - checks if character is a letter both lowercase and uppercase
+ *
+ * @c: takes input from other functions
+ *
+ * Return: 1 if c is ture, else 0
+*/
+
+int _isalpha(int c);
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
