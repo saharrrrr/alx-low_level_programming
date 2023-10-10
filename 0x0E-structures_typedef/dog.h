3992058373958039d4ccefef126e583a979aa89b
saharrrrr <skmao@gmail.com>
2023-10-10 15:48:26 -0700

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOH_H
+#define _DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Decription: dog struct
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
